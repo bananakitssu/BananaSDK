28afb7a6a44f34c8e23f3259db0ca57d9f6e9ad1
bananakitssu <bananacool467+2@gmail.com>
2026-06-08 20:22:01 -0400

    Fix

--- Web/BananaSDK.js
+++ Web/BananaSDK.js
@@ -1,9 +1,9 @@
-export SetBackground_ (color) {
+function SetBackground_ (color) {
     if (!color)
         console.warn("No color, defaulting to black");
     
     try {
-        let new_ = "";
+        let new_;
         
         if (typeof color === "object" && color !== null && !Array.isArray(color)) {
             const hex = "#" + [rgb.r, rgb.g, rgb.b]
@@ -19,6 +19,13 @@ export SetBackground_ (color) {
         }
         
         const html_ = document.querySelector("html");
-        html_.style.backgroundColor = new_;
+        html_.style.width = "100%";
+        html_.style.height = "100%";
+        html_.style.position = "absolute";
+        html_.style.top = "0";
+        html_.style.left = "0";
+        html_.style.backgroundColor = new_ != null ? new_ : "#000000";
     }
 }
+
+export { SetBackground_ };
