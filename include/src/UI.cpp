ba72a30070c5220319c6349620776c3a5c32dc83
bananakitssu <bananacool467+2@gmail.com>
2026-06-11 17:34:32 -0400

    update

--- include/src/UI.cpp
+++ include/src/UI.cpp
@@ -129,9 +129,7 @@ bool UIRenderer::Init(ANativeActivity* activity, std::variant<AndroidApp*, Andro
                 
                 for (auto& element : app_->getElements()) {
                     std::visit([this](auto& visualItem) {
-                        using T = std::decay_t<decltype(*visualItem)>;
-                        if constexpr (!std::is_same_v<T, Textarea>)
-                            visualItem->Draw(*this);
+                        visualItem->Draw(*this);
                         //using ItemType = std::decay_t<decltype(visualItem)>;
                         //const_cast<ItemType&>(visualItem)->Draw(*this);
                     }, element);
